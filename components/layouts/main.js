d3db50998334daef80e892109e91dfb94eef8ab2
cacti23 <patel.aditya2014@gmail.com>
2022-10-07 01:06:34 +0530

    🎨 Update the main layout

---
+++ components/layouts/main.js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import Navbar from '../navbar'
+import { Box, Container } from '@chakra-ui/react'
+import Footer from '../footer'
+import VoxelDogLoader from '../voxel-dog-loader'
+
+const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
+  ssr: false,
+  loading: () => <VoxelDogLoader />
+})
+
+const Main = ({ children, router }) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
+
+        <title>Aditya Patel - Homepage</title>
+      </Head>
+
+      <Navbar path={router.asPath} />
+
+      <Container maxW="container.md" pt={14}>
+        <LazyVoxelDog />
+
+        {children}
+
+        <Footer />
+      </Container>
+    </Box>
+  )
+}
+
+export default Main
